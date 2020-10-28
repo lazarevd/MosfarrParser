cf3cb021f2f4f1180b3c17a553283c1fec5c1139
lazarevd <lazarevd@gmail.com>
2020-10-29 01:28:46 +0300

    save to db

--- sql.c
+++ sql.c
@@ -1,64 +1,98 @@
 #include <stdio.h>
 #include <sqlite3.h>
 #include <string.h>
+#include <stdlib.h>
 #include "newsblock.h"
-int callback(void *, int, char **, char **);
+
+void selectNewsBlocksFromDb(sqlite3 * db, 
+			    struct NewsBlock * nbs,
+			    size_t arrSz) {
+char *sql = "SELECT * FROM news_blocks";
+
+sqlite3_stmt *stmt;
+
+int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+if (rc != SQLITE_OK) {
+    printf("%s%s\n", "error: ", sqlite3_errmsg(db));
+}
+
+int cnt = 0;
+while ((rc = sqlite3_step(stmt)) == SQLITE_ROW && cnt < arrSz) {
+    int id           = sqlite3_column_int (stmt, 0);
+    nbs[cnt].id = id;
+    const char *date = sqlite3_column_text(stmt, 1);
+    strcpy(nbs[cnt].date, date);
+    const char *title = sqlite3_column_text(stmt, 2);
+    strcpy(nbs[cnt].title, title);
+    const char *url = sqlite3_column_text(stmt, 3);
+    strcpy(nbs[cnt].url, url);
+    
+    //printf("%d=%s\n", id, name); 
+    cnt++;
+}
+sqlite3_finalize(stmt);
+
+}
+
+
+int insertNewsBlock(sqlite3 * db, struct NewsBlock * nb) {
+
+char query[4000] = "insert or ignore into news_blocks values(";
+
+char  i[100] = {0};
+sprintf(i, "%d", nb->id);
+strncat(query, &i[0],20);
+strcat(query, ", \"");
+strncat(query, nb->date, 12);
+strcat(query, "\", \"");
+strncat(query, nb->title, 500);
+strcat(query, "\", \"");
+strncat(query, nb->url, 700);
+strcat(query, "\", \"");
+strncat(query, nb->body, 700);
+strcat(query, "\", 0, 0)");
+
+char *err_msg = 0;
+int rc = sqlite3_exec(db, query, 0, 0, &err_msg);
+    
+    if (rc != SQLITE_OK ) {
+        
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        
+        sqlite3_free(err_msg);        
+        sqlite3_close(db);
+        
+        return 1;
+    } 
+
+printf("%s\n", query);
+return rc;
+
+}
 
 int main(void) {
 
 sqlite3 *db;
-sqlite3_stmt *stmt;
+
 char *err_msg = 0; 
 
 int rc = sqlite3_open_v2("rr.db", &db, SQLITE_OPEN_READWRITE,NULL);
-printf("%s\n", sqlite3_libversion());
-printf("%d\n", rc);
-
-char *sql = "SELECT * FROM news_blocks";
-
 size_t arrSz = 20;
-
-
 struct NewsBlock * nbs = malloc(sizeof(struct NewsBlock) * arrSz);
 initNewsBlocks(nbs, arrSz);
+nbs[1].id = -12343545;
+nbs[1].date = "28.10.20";
+nbs[1].title = "tit";
+nbs[1].url = "http://url.com";
 
 
 
-rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-if (rc != SQLITE_OK) {
-    printf("%s%s\n", "error: ", sqlite3_errmsg(db));
-    return 0;
-}
-
-int cnt = 0;
-
-while ((rc = sqlite3_step(stmt)) == SQLITE_ROW && cnt < arrSz) {
-    int id           = sqlite3_column_int (stmt, 0);
-    const char *name = sqlite3_column_text(stmt, 1);
-    strcpy(nbs[cnt].date, name);
-    printf("%d=%s\n", id, name); 
-    cnt++;
-}
-if (rc != SQLITE_DONE) {
-    printf("%s%s\n","error: ", sqlite3_errmsg(db));
-}
-sqlite3_finalize(stmt);
-
-printf("%s=%s\n", "nbs res",nbs[0].date);
-
+//selectNewsBlocksFromDb(db, nbs, arrSz);
+insertNewsBlock(db, &nbs[1]);
+printf("%s%s", "result ", nbs[1].url);
 free(nbs);
 return 0;
 }
 
 
 
-int callback(void *newsBlk, int args, char **argv, char **azColName) {
-
-printf("%s\n", argv[0]);
-for(int i; i<args; i++) {
-printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-}
-printf("\n");
-return 0;
-
-}
