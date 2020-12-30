bf68095b1541408a2fdb2878357b232e62c4c76f
lazarevd <lazarevd@gmail.com>
2020-12-30 23:25:25 +0300

    do

--- fdsarrParser.sh
+++ fdsarrParser.sh
@@ -1,5 +1,5 @@
 rm fdsarrParser
 #for DO
-#gcc -g -o fdsarrParser fdsarrParser.c  newsblock.h -I/usr/include/tidy sql.h sender.h parse.c parse.h sql.c newsblock.c sender.c -L~/sqlite/sqlite -ltidy -lcurl -lsqlite3
-gcc -g -o fdsarrParser fdsarrParser.c  newsblock.h sql.h sender.h parse.c parse.h sql.c newsblock.c sender.c -L~/sqlite/sqlite -ltidy -lcurl -lsqlite3
+gcc -g -o fdsarrParser fdsarrParser.c  newsblock.h -I/usr/include/tidy sql.h sender.h parse.c parse.h sql.c newsblock.c sender.c -L~/sqlite/sqlite -ltidy -lcurl -lsqlite3
+#gcc -g -o fdsarrParser fdsarrParser.c  newsblock.h sql.h sender.h parse.c parse.h sql.c newsblock.c sender.c -L~/sqlite/sqlite -ltidy -lcurl -lsqlite3
 ./fdsarrParser
